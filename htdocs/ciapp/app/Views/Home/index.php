bff916ba644f3cbdec8f39f4f164b3e542232d44
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-05-05 12:22:55 +0530

    Recreate ciapp

--- htdocs/ciapp/app/Views/Home/index.php
+++
@@ -1,4 +0,0 @@
-<?= $this->extend('layouts/default') ?>
-
-<?= $this->section('title'); ?>Home<?= $this->endSection(); ?>
-<?= $this->section('content'); ?><h1>Home Page Loaded</h1><?= $this->endSection(); ?>
